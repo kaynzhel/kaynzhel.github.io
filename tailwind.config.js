6353190509b0a03bfbe17c2e07b161dd433e63c8
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-06-20 20:05:01 -0600

    update font and metadata

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -11,6 +14,15 @@ module.exports = {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        mont: ["var(--font-mont)", ...fontFamily.sans],
+      },
+      colors: {
+        dark: "#222222",
+        light: "#f5f5f5",
+        primary: "#682860",
+        primaryDark: "#0B1C2B",
       },
     },
   },
