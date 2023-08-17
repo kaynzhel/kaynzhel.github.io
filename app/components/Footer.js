d0f8d8a79abe9c535c29e3a5c82ddbb94a9ababd
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2023-08-17 01:29:05 -0600

    V2: Add responsive styling for mobile devices (#7)

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -4,11 +4,11 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className={"w-full border-t-2 border-solid border-dark font-medium text-lg text-dark"}>
-            <GenLayout className={"py-8 flex items-center justify-between"}>
+        <footer className={"w-full border-t-2 border-solid border-dark font-medium text-lg text-dark sm:text-base"}>
+            <GenLayout className={"py-8 flex items-center justify-between lg:flex-col lg:py-6"}>
                 <span>{new Date().getFullYear()} &copy; All Rights Reserved.</span>
-                <div className={"flex items-center"}>
-                    <span className={"text-2xl text-primary"}>&#9825;</span>
+                <div className={"flex items-center lg:py-2"}>
+                    <span className={"text-2xl text-primary sm:text-base"}>&#9825;</span>
                     <p className={"underline underline-offset-2 mx-2"}>Kaye Ena Crayzhel Misay</p>
                 </div>
                 <Link
