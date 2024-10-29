a14bcc2847dd5ffcea9807d89b899bf82d259247
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2024-10-29 00:34:55 -0600

    update GenLayout sizing and padding

--- app/components/GenLayout.js
+++ app/components/GenLayout.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const GenLayout = ({children, className=""}) => {
-    return (
-        <div className={`${className} w-full h-full inline-block p-32 xl:p-24 lg:p-16 md:p-12 sm:p-8`}>
-            {children}
-        </div>
-    )
+  return (
+    <div className={`${className} w-full h-full inline-block px-32 xl:px-24 lg:px-16 md:px-12 sm:px-8`}>
+      {children}
+    </div>
+  )
 }
 
-export default GenLayout
+export default GenLayout;
