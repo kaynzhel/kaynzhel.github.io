805cbf2141e1c781c049d58d5884f5aa7434d209
Kaye Ena Crayzhel Misay <crayzhel_misay@yahoo.com>
2024-10-29 03:24:09 -0600

    spacing and logo changes

--- app/components/Logo.js
+++ app/components/Logo.js
@@ -7,9 +7,9 @@ const MotionLink = motion(Link);
 
 const Logo = () => {
   return (
-    <div className={"container flex items-center justify-center mt-2"}>
+    <div className={"container flex items-center justify-center mt-2 w-12 h-12"}>
       <MotionLink
-        className={"w-16 h-16 bg-dark text-light flex items-center justify-center rounded-full text-2xl font-bold"}
+        className={"w-full h-full bg-dark text-light flex items-center justify-center rounded-full text-xl font-bold"}
         href={"/"}
         whileHover={{
           backgroundColor: ["#2B004E", "#1D0E27", "#5A2A79", "#2B004E", "#682860", "#2B004E"],
